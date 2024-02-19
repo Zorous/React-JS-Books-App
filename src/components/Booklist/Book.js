bb143da2107891b106a71081f789ec38f70557e0
Zorous <oussamabelhadi5@gmail.com>
2024-02-19 14:27:10 +0100

    fixing blank page bug

--- src/components/Booklist/Book.js
+++ src/components/Booklist/Book.js
@@ -17,7 +17,8 @@ function Book(book){
 
         <div className='book-item-info-item author fs-15'>
           <span className='text-capitalize fw-7'>Author: </span>
-          <span>{book.author.join(", ")}</span>
+          <span>{book.author && book.author.length > 0 ? book.author.join(", ") : null}</span>
+
         </div>
 
         <div className='book-item-info-item edition-count fs-15'>
